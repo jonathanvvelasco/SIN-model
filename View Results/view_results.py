6d7d9b3cc156dcb73f09ff218366d9cd570d3161
Jonathan Veiga Velasco <167229177+jonathanvvelasco@users.noreply.github.com>
2026-01-04 18:33:01 -0300

    Primeiro resultado com emissões
    Emissão total de 34 MtCO2 ao fim do horizonte. As emissões ocorrem apenas em 2050 por conta de gás natural.

--- View Results/view_results.py
+++ View Results/view_results.py
@@ -29,57 +29,74 @@ from message_ix.util.tutorial import prepare_plots
 rep = Reporter.from_scenario(base)
 prepare_plots(rep)
 
+# filter="filter_all_tecs"
+filter="filter_emission_tecs"
+# %% Define Filters
+
+if filter=="filter_all_tecs":
+    rep.set_filters(t=["batt_4_n",
+        "batt_4_ne",
+        "batt_4_s",
+        "batt_4_se",
+        "bio_ppl",
+        "gas_ppl",
+         "gas_ppl_1",
+         "gas_ppl_2",
+         "gas_ppl_ccs",
+         "gas_ppl_ccs_1",
+         "gas_ppl_ccs_2",
+         "coal_ppl",
+         "nuc_ppl",
+         "oil_ppl",
+         "solar_pv_ppl",
+         "hydro_4",
+         "hydro_8",
+         "hydro_9",
+         "hydro_3",
+         "hydro_1",
+         "hydro_5",
+         "hydro_6",
+         "hydro_7",
+         "hydro_10",
+         "hydro_12",
+         "hydro_2",
+         "hydro_11",
+         "pump_sphs_4",
+         "pump_sphs_8",
+         "pump_sphs_9",
+         "pump_sphs_3",
+         "pump_sphs_1",
+         "pump_sphs_6",
+         "pump_sphs_7",
+         "pump_sphs_10",
+         "pump_sphs_12",
+         "pump_sphs_2",
+         "pump_sphs_11",
+         "wind_ppl",
+         "wind_ppl_cos",
+         "wind_ppl_int",
+         "wind_ppl_rs"
+         ])
+elif filter=="filter_emission_tecs":
+    rep.set_filters(t=["bio_ppl",
+        "gas_ppl",
+        "gas_ppl_1",
+        "gas_ppl_2",
+        "gas_ppl_ccs",
+        "gas_ppl_ccs_1",
+        "gas_ppl_ccs_2",
+        "coal_ppl",
+        "nuc_ppl",
+        "oil_ppl",
+        ])
+    
 # %% Report Activity and Capacity
-rep.set_filters(t=["batt_4_n",
-                   "batt_4_ne",
-                   "batt_4_s",
-                   "batt_4_se",
-                   "bio_ppl",
-                   "gas_ppl",
-                    "gas_ppl_1",
-                    "gas_ppl_2",
-                    "gas_ppl_ccs",
-                    "gas_ppl_ccs_1",
-                    "gas_ppl_ccs_2",
-                    "coal_ppl",
-                    "nuc_ppl",
-                    "oil_ppl",
-                    "solar_pv_ppl",
-                    "hydro_4",
-                    "hydro_8",
-                    "hydro_9",
-                    "hydro_3",
-                    "hydro_1",
-                    "hydro_5",
-                    "hydro_6",
-                    "hydro_7",
-                    "hydro_10",
-                    "hydro_12",
-                    "hydro_2",
-                    "hydro_11",
-                    "pump_sphs_4",
-                    "pump_sphs_8",
-                    "pump_sphs_9",
-                    "pump_sphs_3",
-                    "pump_sphs_1",
-                    "pump_sphs_6",
-                    "pump_sphs_7",
-                    "pump_sphs_10",
-                    "pump_sphs_12",
-                    "pump_sphs_2",
-                    "pump_sphs_11",
-                    "wind_ppl",
-                    "wind_ppl_cos",
-                    "wind_ppl_int",
-                    "wind_ppl_rs"
-                    ])
 rep.get("plot activity")
 rep.get("plot capacity")
 rep.get("plot new capacity")
 rep.set_filters(c=["electricity"]) # Somente commodity eletricidade
 rep.get("plot prices")
 rep.get("plot demand")
-# plt.show()
 
 # %% Compares production and demand
 out = rep.full_key("out")
@@ -111,6 +128,10 @@ plt.tight_layout()
 plt.rcParams['font.size'] = 18
 plt.grid(axis='y')
 plt.show()
+
+# %% Plot emissions
+emissions = base.var("EMISS", {"node": "Southeast"})
+emissions
 
 # %% Close DB
 mp.close_db()# -*- coding: utf-8 -*-
