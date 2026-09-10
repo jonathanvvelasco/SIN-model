b7b68f165e2748dfbd533208e38c2e2c35939966
Jonathan Veiga Velasco <167229177+jonathanvvelasco@users.noreply.github.com>
2026-09-10 13:55:13 -0300

    Debug general imports

--- step_results/__init__.py
+++ step_results/__init__.py
@@ -1,5 +1,8 @@
 try:
     from .view_results import *
+except ImportError:
+    print("Failed to import view_results. In case of issues, check if the necessary modules are installed.")
+try:
     from .view_sankey import *
 except ImportError:
-    from .view_results import *
+    print("Failed to import view_sankey. In case of issues, check if the necessary modules are installed.")
